8d37c68f2f81b3e123de747c139dce3f79e4692a
Ryan Mower <ryancmower@gmail.com>
2022-04-24 21:45:15 -0500

    Database is hooked up and working

--- index.js
+++ index.js
@@ -20,23 +20,34 @@ const app = express();   // Create an express application
 // ===== Middleware =====
 app.use(bodyparser.urlencoded({extended: true})); // apply the body-parser middleware to all incoming requests
 app.use(bodyparser.json()); 
-//app.use('/client', express.static(__dirname + '/client')); // middle ware to serve static files
 app.use(express.static(path.join(__dirname, "public"))); // middle ware to serve static files
 app.listen(port, () => console.log('Listening on port', port)); // server listens on port set to value above for incoming connections
 console.log("http://localhost:" + port);
 
-
-// Reads in Express Configs
+var json_config;
+var connection;
+// Reads in Express Configs and Connects to MySQL database
 fs.readFile("./configs/secrets.json", function(err, data){
   if (err) throw err;
-  var json_data = JSON.parse(data);
+  json_config = JSON.parse(data);
   app.use(session({
-    secret: json_data["express_key"],
+    secret: json_config["express_key"],
     saveUninitialized: true,
     //cookie: { maxAge: oneDay },
     resave: false
   }));
+
+  // Connect to MySQL Database
+  connection = mysql.createPool({
+    host:     json_config["dbconfig"]["host"],
+    user:     json_config["dbconfig"]["user"],
+    password: json_config["dbconfig"]["password"],
+    database: json_config["dbconfig"]["database"],
+    port:     parseInt(json_config["dbconfig"]["port"])
+  });
 });
+
+
 
 // ===== Routes ======
 // Default landing page
@@ -45,12 +56,18 @@ app.get('/', function(req, res) {
 });
 
 // Default landing page
-app.get('index.html', function(req, res) {
+app.get('/index.html', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 // Login page
-app.get('login', function(req, res) {
+app.get('/login', function(req, res) {
+  // Run generated query     
+  connection.query("SELECT * FROM users;", function(err, results) {    
+    // Error Occured    
+    if (err) throw err;
+    console.log(results);
+  });
   res.sendFile(__dirname + '/public/login.html');
 });
 
