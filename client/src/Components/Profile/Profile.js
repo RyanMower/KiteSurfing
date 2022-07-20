46eb8fa3a50d0a525f9d42cd3541149be0f0b6cd
Ryan Mower <ryancmower@gmail.com>
2022-07-19 22:03:31 -0500

    Profile editor view can now show input fields to change first and last name and phone number. Still have to finish submit function.

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const navigate = useNavigate();
+  const [editView, setEditView] = useState(false);
+  const [page, setPage] = useState();
   const [data, setData] = useState({
     email: "",
     fname: "",
@@ -26,6 +28,34 @@ function Profile() {
 
   }
 
+  function handleSubmit(){
+    // SUVMIT DATA TO BACKEND HERE
+    // TODO
+
+  }
+
+  function toggleProfileEditor(){
+    setEditView(!editView);
+  }
+  function handleChangeFname(event) {
+     setData({
+     ...data,
+     fname: event.target.value
+   });
+  }
+  function handleChangeLname(event) {
+    setData({
+     ...data,
+     lname: event.target.value
+   });
+  }
+  function handleChangeNumber(event) {
+    setData({
+     ...data,
+     number: event.target.value
+   });
+  }
+
   useEffect(() => {
     fetch("/getLoggedInUserInfo", {
       method: "GET",
@@ -34,7 +64,6 @@ function Profile() {
       .then(resp => resp.json())
       .then(data => {
         if(data.status === "success"){
-          console.log(data);
           let ret_data  = {
               email: data["user_email"],
               fname: data["user_fname"],
@@ -42,52 +71,94 @@ function Profile() {
               number: data["user_phone"],
           };
           setData(ret_data);
+
         }
         })
       .catch(err => console.log(err));
 
-  }, [])
+  }, [editView]);
 
 
-  return (
-    <div className="Profile">
-      <Container>
-      <Row>
-        <Col>
-         Email 
-        </Col>
-        <Col>
-         {data["email"]}
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          First Name 
-        </Col>
-        <Col>
-         {data["fname"]} 
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          Last Name 
-        </Col>
-        <Col>
-         {data["lname"]} 
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-         Phone Number 
-        </Col>
-        <Col>
-         {data["number"]} 
-        </Col>
-      </Row>
-    </Container>
-    <Button onClick={logout}>Logout</Button>
-    </div>
-  );
+  if(editView){
+    return (
+      <div className="Profile">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+            Email:
+            </label>
+            <label>
+              {data["email"]}
+            </label>
+          </div>
+          <div>
+            <label>
+            First Name:
+            <input type="text" value={data["fname"]} onChange={handleChangeFname}/>
+            </label>
+          </div>
+          <div>
+            <label>
+            Last Name:
+            <input type="text" value={data["lname"]} onChange={handleChangeLname}/>
+            </label>
+          </div>
+          <div>
+            <label>
+            Phone Number:
+            <input type="text" value={data["number"]} onChange={handleChangeNumber}/>
+            </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+        <Button onClick={toggleProfileEditor}>Edit profile</Button>
+      </div>
+
+    );
+  }
+  else{
+    return (
+      <div className="Profile">
+        <Container>
+        <Row>
+          <Col>
+           Email 
+          </Col>
+          <Col>
+           {data["email"]}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            First Name 
+          </Col>
+          <Col>
+           {data["fname"]} 
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Last Name 
+          </Col>
+          <Col>
+           {data["lname"]} 
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+           Phone Number 
+          </Col>
+          <Col>
+           {data["number"]} 
+          </Col>
+        </Row>
+      </Container>
+      <Button onClick={logout}>Logout</Button>
+      <Button onClick={toggleProfileEditor}>Edit profile</Button>
+      </div> 
+    );
+  }
+  
 }
 
 export default Profile;
